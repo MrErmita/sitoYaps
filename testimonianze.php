272317036d8988e4823c467c7974afff9e3e4d01
MrErmita <alessio.bragetti@gmail.com>
2025-04-13 17:24:35 +0200

    creazione scheletro

---
+++ testimonianze.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testimonianze</title>
+</head>
+<body>
+    <h1>Testimonianze</h1>
+    <p>Qui ci sono le testimonianze</p>
+</body>
+</html>
